f7c518ddbbdd3d83141efc7284ab97c1b76c631a
Angelina <stepanovangelina6913@gmail.com>
2022-10-05 23:01:22 +0300

    add HWTask15/Program.cs

--- HWTask15/Program.cs
+++ HWTask15/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int daynumber = ReadInt("Введите число от 1 до 7: ");
+Console.WriteLine(WorkHoliday(daynumber));
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+string WorkHoliday(int a)
+{
+    if (a > 0 && a < 8)
+    {
+        if (a == 7 || a == 6)
+        {
+            Console.Write("да, выходной");
+        }
+        else
+        {
+            Console.Write("нет, рабочий");
+        }
+    }
+    else
+    {
+        Console.Write("Вы ввели число не в пределах от 1 до 7, поэтому не возможно определить");
+    }
+    return " день.";
+}
