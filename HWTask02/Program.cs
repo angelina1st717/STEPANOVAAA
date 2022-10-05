a355e6cb6b24332ae56f50d166406331f635d851
Angelina <stepanovangelina6913@gmail.com>
2022-10-05 14:16:32 +0300

    add HWTask02/Program.cs

--- HWTask02/Program.cs
+++ HWTask02/Program.cs
@@ -4,3 +4,27 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+{
+Console.Write("Введи два числа и я тебе покажу какое из них больше.");
+int a, b;
+Console.WriteLine("Введите первое целое число: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+bool x = a > b;
+bool y = a < b;
+bool z = a == b; 
+if (x)
+{
+  Console.WriteLine($"{a}");  
+}
+else if (y)
+{
+    Console.WriteLine($"{b}");
+}
+else if (z)
+{
+    Console.WriteLine($"равны");
+}
+}
