985034c5f02f831e367cc8ab5a78f7de4c71f31b
Angelina <stepanovangelina6913@gmail.com>
2022-10-05 16:38:56 +0300

    add HWTask08/Program.cs

--- HWTask08/Program.cs
+++ HWTask08/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int N = a;
+int counter = 1;
+
+while (counter <= N)
+{
+    if (counter % 2 ==0)
+    {
+        Console.WriteLine($"{counter}");
+        counter++;
+    }
+    else
+    {
+       counter++;
+    }
+}
+
+
