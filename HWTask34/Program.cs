e559cfc1eaf4f5ba39aac7c77b3e84222b761bc0
Angelina <stepanovangelina6913@gmail.com>
2022-10-10 16:56:59 +0300

    HWTask34/Program.cs

--- HWTask34/Program.cs
+++ HWTask34/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+Random rnd = new Random();
+int[] array = new int[4];
+int count = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(99, 999);
+    if((array[i] % 2) == 0)
+    count ++;
+}
+var result = string.Join(", ", array);
+Console.WriteLine($"[{result}]");
+Console.WriteLine($"{count}");
