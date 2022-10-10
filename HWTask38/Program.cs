36e9790cc1c6e15b3ff2b364f3104338267d6110
Angelina <stepanovangelina6913@gmail.com>
2022-10-10 18:09:44 +0300

    HWTask38/Program.cs

--- HWTask38/Program.cs
+++ HWTask38/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементами массива.
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+
+
+double[] CreateArrayRndInt(int size, int min, int max)
+{
+    double[] array = new double[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+      array[i] = Math.Round(rnd.NextDouble() * (max - min) + min, 2);  
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    int size = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < size; i++)
+    {
+        if (i < size - 1) Console.Write($"{array[i]}, "); 
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+double[] MinMax(double[] array)
+{
+    double max = default;
+    double min = default;
+
+    int size = array.Length;
+    for(int i = 0; i < size; i = i + 1)
+    {
+        if(array[i] > array[i + 1])
+        {
+            max = array[i];
+            min = array[i + 1];
+        }
+        else
+        {
+            max = array[i + 1];
+            min = array[i];
+        }
+    }
+    return new double[] {max, min};
+}
