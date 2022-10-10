ee21290455f00bffe4a05dced3f8c2f663cab0f7
Angelina <stepanovangelina6913@gmail.com>
2022-10-10 13:39:12 +0300

    HWTask27/Program.cs

--- HWTask27/Program.cs
+++ HWTask27/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (number > 0)
+{
+    int num = number % 10;
+    number = number / 10;
+    sum = sum + num;
+}
+Console.WriteLine(sum);
