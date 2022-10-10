4c8280ea9f9ffa63ee3ed3426a7426570591745a
Angelina <stepanovangelina6913@gmail.com>
2022-10-10 17:24:43 +0300

    HWTask36/Program.cs

---
+++ HWTask36/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Random rnd = new Random();             
+int[] array = new int[4];              
+int summ = 0;                          
+for (int i = 0; i < array.Length; i++) 
+{
+    array[i] = rnd.Next(0, 99);        
+    if (i % 2 != 0)                    
+    {
+        summ = summ + array[i];        
+    }
+}
+var result = string.Join(", ", array);              
+Console.WriteLine($"[{result}]");
+Console.WriteLine($"{summ}");
+
