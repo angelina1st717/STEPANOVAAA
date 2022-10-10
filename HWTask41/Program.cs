53166221fa82197c560e1d18fb91cefe0ae69c9a
Angelina <stepanovangelina6913@gmail.com>
2022-10-10 18:31:01 +0300

    HWTask41/Program.cs

--- HWTask41/Program.cs
+++ HWTask41/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите числа через запятую: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(','), int.Parse); 
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"{count}");
