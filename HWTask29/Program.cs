c958bde5c7a45e47cf2cac3871b324030763bae7
Angelina <stepanovangelina6913@gmail.com>
2022-10-10 14:53:58 +0300

    HWTask29/Program.cs

--- HWTask29/Program.cs
+++ HWTask29/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+Console.Write("Введите 8 элементов массива: ");
+{  
+    int[] array = new int[8]; 
+    int i;  
+    for(i=0; i<8; i++)  
+    {  
+	    array[i] = Convert.ToInt32(Console.ReadLine());  		
+    }   
+     Console.WriteLine($"[{String.Join(",",array)}]");
+}
+   
