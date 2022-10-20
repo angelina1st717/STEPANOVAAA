72c9b3b473300f5395a3c8215bc8341c6c4a3dd9
Angelina <stepanovangelina6913@gmail.com>
2022-10-21 02:31:44 +0300

    FINALTASK/Program.cs

--- FINALTASK/Program.cs
+++ FINALTASK/Program.cs
@@ -8,51 +8,25 @@ string[] secondArray = new string[firstArray.Length];
 
 void NewArray(string[] firstArray, string[] secondArray)
 {
-
     int count = 0;
 
     for (int i = 0; i < firstArray.Length; i++)
-
     {
-
-
         if (firstArray[i].Length <= 3)
-
-
         {
-
-
-
             secondArray[count] = firstArray[i];
-
-
-
             count++;
-
-
-
         }
-
-
     }
-
 }
 
 void PrintArray(string[] array)
 {
-
     for (int i = 0; i < array.Length; i++)
-
     {
-
-
         Console.Write($"{array[i]} ");
-
-
     }
-
     Console.WriteLine();
-
 }
 
 NewArray(firstArray, secondArray);
