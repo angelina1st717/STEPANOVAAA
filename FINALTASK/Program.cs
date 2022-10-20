03aaa96848982852eb7dda67d22ba111af442f75
Angelina <stepanovangelina6913@gmail.com>
2022-10-21 01:40:42 +0300

    FINALTASK/Program.cs

---
+++ FINALTASK/Program.cs
@@ -0,0 +1,32 @@
+﻿//Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+//длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+//либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+//лучше обойтись исключительно массивами.
+
+string[] firstArray = new string[5] { "Hello", "my", "friend", "what's", "up" };
+string[] secondArray = new string[firstArray.Length];
+
+void NewArray(string[] firstArray, string[] secondArray)
+{
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= 3)
+        {
+            secondArray[count] = firstArray[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+NewArray(firstArray, secondArray);
+PrintArray(secondArray);
