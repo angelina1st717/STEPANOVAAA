e4f4a4a8dad96ed6e8a1f824edb631c1eb7b7583
Angelina <stepanovangelina6913@gmail.com>
2022-10-21 02:14:57 +0300

    FINALTASK/Program.cs

--- FINALTASK/Program.cs
+++ FINALTASK/Program.cs
@@ -3,29 +3,56 @@
 //либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 //лучше обойтись исключительно массивами.
 
-string[] firstArray = new string[5] { "Hello", "my", "friend", "what's", "up" };
+string[] firstArray = new string[5] { "Hello", "473", "world", "12", "hey" };
 string[] secondArray = new string[firstArray.Length];
 
 void NewArray(string[] firstArray, string[] secondArray)
 {
+
     int count = 0;
+
     for (int i = 0; i < firstArray.Length; i++)
+
     {
+
+
         if (firstArray[i].Length <= 3)
+
+
         {
+
+
+
             secondArray[count] = firstArray[i];
+
+
+
             count++;
+
+
+
         }
+
+
     }
+
 }
 
 void PrintArray(string[] array)
 {
+
     for (int i = 0; i < array.Length; i++)
+
     {
+
+
         Console.Write($"{array[i]} ");
+
+
     }
+
     Console.WriteLine();
+
 }
 
 NewArray(firstArray, secondArray);
